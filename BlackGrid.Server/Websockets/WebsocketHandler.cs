53b06d4f3673fe5201bbbc5f84dd4dd28c0e89ac
jusoaresg <julianosgreg@gmail.com>
2026-02-22 01:37:07 -0300

    remove(handler): comment unused matchmaking service

--- BlackGrid.Server/Websockets/WebsocketHandler.cs
+++ BlackGrid.Server/Websockets/WebsocketHandler.cs
@@ -7,21 +7,21 @@ namespace BlackGrid.Server.Websockets;
 
 public class WebsocketHandler
 {
-	private readonly MatchmakingService _matchmaking = new();
-
-	public async Task HandleConnection(WebSocket socket)
-	{
-		var session = new PlayerSession(socket);
-
-		var match = _matchmaking.TryCreateMatch(session);
-		if (match is not null)
-		{
-			Match _match = match;
-			await MatchNotifier.BroadcastMatchStart(_match);
-			await MatchNotifier.BroadcastGameState(_match);
-		}
-
-		await WebsocketReceiver.ReceiveLoop(session);
-	}
-
+	// private readonly MatchmakingService _matchmaking = new();
+	//
+	// public async Task HandleConnection(WebSocket socket)
+	// {
+	// 	var session = new PlayerSession(socket);
+	//
+	// 	var match = _matchmaking.TryCreateMatch(session);
+	// 	if (match is not null)
+	// 	{
+	// 		Match _match = match;
+	// 		await MatchNotifier.BroadcastMatchStart(_match);
+	// 		await MatchNotifier.BroadcastGameState(_match);
+	// 	}
+	//
+	// 	await WebsocketReceiver.ReceiveLoop(session);
+	// }
+	//
 }
