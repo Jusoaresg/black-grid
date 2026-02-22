eb838bf0d699dd059ddca6efeca0275550a285f1
jusoaresg <julianosgreg@gmail.com>
2026-02-22 01:39:07 -0300

    feat(factory): players has different shuffled deck

--- BlackGrid.Game/GameFactory.cs
+++ BlackGrid.Game/GameFactory.cs
@@ -11,20 +11,18 @@ public static class GameFactory
 {
 	public static GameState Create(GameConfig config)
 	{
-		//var dataRoot = Path.Combine(AppContext.BaseDirectory, "..", "BlackGrid.Data");
-
 		var cardDb = new CardDatabase(Path.Combine(config.DataRootPath, "Cards"));
 		var deckDb = new DeckDatabase(Path.Combine(config.DataRootPath, "Decks"), cardDb);
 
 		var deckDef = deckDb.Get("DECK_CORE_PRESSURE");
 		var instances = deckDef.CardIds.Select(id => new CardInstance(Guid.NewGuid().ToString(), cardDb.Get(id)));
-		instances.Shuffle();
-		var deck = new Deck(instances);
+		var deck1 = new Deck(instances.Shuffle());
+		var deck2 = new Deck(instances.Shuffle());
 
 		//NOTE: Change player creation
-		Player player1 = new Player(0, deck, new PlayerBoard());
-		Player player2 = new Player(1, deck, new PlayerBoard());
+		Player player1 = new(0, deck1, new PlayerBoard());
+		Player player2 = new(1, deck2, new PlayerBoard());
 
-		return new GameState(config, new Player[] { player1, player2 });
+		return new GameState(config, [player1, player2]);
 	}
 }
